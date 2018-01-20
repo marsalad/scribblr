d4198d76420de3a8c7a17da209e217bd649f8d91
Kerlin Michel <michelkerlin@gmail.com>
2018-01-20 03:02:07 -0500

    Added socket-io to trigger record remotely

--- server/server.py
+++ server/server.py
@@ -1,13 +1,21 @@
 from flask import Flask, request
+import socketio
+import eventlet
+import eventlet.wsgi
 import speech_recognition as sr
 from pydub import AudioSegment
 
 app = Flask(__name__)
+sio = socketio.Server()
 r = sr.Recognizer()
 
-@app.route("/test", methods=["POST"])
-def test():
-    return "Hello"
+recorders = []
+
+@app.route("/start-recording", methods=["POST", "GET"])
+def trigger_record():
+    for recorder in recorders:
+        sio.emit('start-recording', room=recorder)
+    return "Success"
 
 @app.route("/stream-audio", methods=["POST"])
 def stream_audio():
@@ -27,5 +35,22 @@ def stream_audio():
         return 'Got file'
     return 'No file'
 
+@sio.on('connect')
+def connect(sid, environ):
+    recorders.append(sid)
+    print("connect ", sid)
+
+@sio.on('chat message')
+def message(sid, data):
+    print("message ", data)
+    sio.emit('reply', room=sid)
+
+@sio.on('disconnect')
+def disconnect(sid):
+    recorders.remove(sid)
+    print('disconnect ', sid)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
