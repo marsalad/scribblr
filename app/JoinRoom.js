d92f58949b0685343c2e4a6d15ddc33a6ff13807
Diana Marsala <marsalad@sas.upenn.edu>
2018-01-20 17:20:02 -0500

    Update JoinRoom.js

--- app/JoinRoom.js
+++ app/JoinRoom.js
@@ -1,79 +1,76 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput,
-  TouchableHighlight, Modal, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
 export default class JoinRoom extends React.Component {
 
   state = {
-    joinRoomModalVisible: false,
-    roomName: ''
+    recording: false,
+    roomName: '',
+    graphicIndex: 0
   }
-
-  joinRoom() {
-    console.log('Choosing Room')
-    const { navigate } = this.props.navigation;
-    navigate('RoomControls', { roomName: this.state.roomName })
-  }
-
-  openJoinRoom() {
-    this.setState({joinRoomModalVisible:true});
-  }
-
-  closeJoinRoom() {
-    this.setState({joinRoomModalVisible:false});
-  }
-
 
   render() {
     return (
       <View style={styles.container}>
-        <Modal
-            visible={this.state.joinRoomModalVisible}
-            animationType={'slide'}
-            onRequestClose={() => this.closeJoinRoom()}
-        >
-          <View style={styles.modalContainer}>
-            <View style={styles.innerContainer}>
-              <Text>Input name of room</Text>
-              <TextInput
-                style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 300}}
-                onChangeText={(roomName) => this.setState({roomName})}
-                value={this.state.roomName}
-                placeholder="Room Name"
-              />
-              <Button
-                  onPress={() => {this.closeJoinRoom(); this.joinRoom();}}
-                  title="Join room"
-              />
-              <Button
-                  onPress={() => {this.closeJoinRoom()}}
-                  title="Cancel"
-              />
-            </View>
+        <View style={styles.topContainer}>
+          <View style={styles.buffer}></View>
+          <TextInput
+            style={styles.roomButton}
+            underlineColorAndroid='transparent'
+            placeholderTextColor='#280602'
+            onChangeText={(roomName) => this.setState({roomName})}
+            value={this.state.roomName}
+            placeholder='< Room ID >'
+          />
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.animation}>
+            <Image
+              style={{width: 350, height: 350}}
+              source={{uri: graphics[this.state.graphicIndex]}}
+            />
           </View>
-        </Modal>
-        <Text>Scribblr</Text>
-        <TouchableHighlight onPress={() => this.openJoinRoom()}>
-          <Text>Join Room</Text>
-        </TouchableHighlight>
+        </View>
       </View>
     );
   }
 }
 
+const graphics = [
+  'https://preview.ibb.co/bwTABw/rings.png',
+  'https://media.giphy.com/media/3ohc11ZVEOdkJCFffG/giphy.gif'
+]
+
 const styles = StyleSheet.create({
+  buffer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#222e38'
   },
-  modalContainer: {
-    flex: 1,
+  topContainer: {
+    flex: 1.2,
+    justifyContent: 'center'
+  },
+  bottomContainer: {
+    flex: 3,
+    justifyContent: 'center'
+  },
+  roomButton: {
+    flex: 0.4,
+    color: '#280602',
+    borderRadius: 7,
+    borderColor: '#280602',
+    borderWidth: 1,
+    textAlign: 'center',
     justifyContent: 'center',
-    backgroundColor: 'grey',
-  },
-  innerContainer: {
     alignItems: 'center',
+    backgroundColor: 'silver',
+    fontSize: 25,
+    width: 280
   }
 });
