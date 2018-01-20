1a7793c17bd5577e088aa2f9f30bcdffbbebf1c2
Kerlin Michel <michelkerlin@gmail.com>
2018-01-20 02:27:13 -0500

    Endpoint that takes in audio file and gets text from it

--- server/server.py
+++ server/server.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
+import speech_recognition as sr
+from pydub import AudioSegment
+
 app = Flask(__name__)
+r = sr.Recognizer()
 
 @app.route("/test", methods=["POST"])
 def test():
@@ -9,7 +13,17 @@ def test():
 def stream_audio():
     if 'file' in request.files:
         _file = request.files['file']
-        _file.save('./test.aac')
+        _file.save('./test.wav')
+        audio_file = AudioSegment.from_file("test.wav", format="m4a")
+        audio_file.export("test.wav", format="wav")
+        try:
+            with sr.AudioFile('./test.wav') as f:
+                audio = r.record(f)
+                text = r.recognize_google(audio)
+                print(text)
+        except Exception as e:
+            print(e)
+            return 'Error'
         return 'Got file'
     return 'No file'
 
